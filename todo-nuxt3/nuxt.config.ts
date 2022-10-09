a126cbb0e23b909134437620f9c2c93af1c153a2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-08 23:28:19 -0600

    enable hmr on nuxt inside docker

--- todo-nuxt3/nuxt.config.ts
+++ todo-nuxt3/nuxt.config.ts
@@ -3,9 +3,19 @@ const cacheDir =
     ? '/node/node_modules/.vite'
     : 'node_modules/.vite';
 
+const hmrPort =
+  process.env.STAGE === 'development-docker'
+    ? +process.env.HMR_PORT
+    : undefined;
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   vite: {
     cacheDir,
+    server: {
+      hmr: {
+        port: hmrPort,
+      },
+    },
   },
 });
