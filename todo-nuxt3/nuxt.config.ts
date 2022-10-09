2372adcde998944bdacad6b5691f0b554536108d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-08 23:04:07 -0600

    dockerize attempt 3

--- todo-nuxt3/nuxt.config.ts
+++ todo-nuxt3/nuxt.config.ts
@@ -1,4 +1,11 @@
+const cacheDir =
+  process.env.STAGE === 'development-docker'
+    ? '/node/node_modules/.vite'
+    : 'node_modules/.vite';
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  vite: {
+    cacheDir,
+  },
+});
