4a054d3e593fe194bb7ab21c08d62ebb6163aff4
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2023-05-26 17:46:51 -0600

    delete cacheDir flag and required setup

--- todo-nuxt3/nuxt.config.ts
+++ todo-nuxt3/nuxt.config.ts
@@ -1,14 +1,9 @@
-const cacheDir =
-  process.env.STAGE === 'development-docker'
-    ? '/node/node_modules/.vite'
-    : 'node_modules/.vite';
-
 const hmrPort =
   process.env.STAGE === 'development-docker'
     ? +(process.env.HMR_PORT || '3001')
     : undefined;
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: {
     strict: true,
@@ -19,7 +14,6 @@ export default defineNuxtConfig({
     },
   },
   vite: {
-    cacheDir,
     server: {
       hmr: {
         port: hmrPort,
